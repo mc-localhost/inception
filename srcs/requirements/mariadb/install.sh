d3b37a380f65f55e33b7eb4acad0eab85e320509
Viktoriia Vasiukova <vvasiuko@student.42heilbronn.de>
2025-06-17 15:41:37 +0200

    mariadb: install.sh replace root with mysql, added --user=mysql everywhere, left log messages uncommented

--- srcs/requirements/mariadb/install.sh
+++ srcs/requirements/mariadb/install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=root --datadir=/var/lib/mysql
-    mysqld_safe --skip-networking &
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysqld_safe --user-mysql &
     pid="$!"
-    # waiting for mysqladmin to run
-    until mysqladmin ping --silent --user=root; do
+    until mysqladmin -u root ping -h localhost --silent; do
+        echo "waiting for mysqladmin to run..."
         sleep 1
     done
 
     envsubst < /etc/mysql/init_env.sql > /etc/mysql/init.sql
     # to check in docker logs mariadb that substitution worked correctly 
-    # echo "after substitution:"
-    # cat /etc/mysql/init.sql
+    echo "after substitution:"
+    cat /etc/mysql/init.sql
     mysql < /etc/mysql/init.sql
 
-    mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
+    mysqladmin -u root -p${DB_ROOT_PASS} shutdown
     # wait for mysqld to be done, only to restart later in the code
     wait "$pid"
 fi
 
-exec mysqld_safe
+exec mysqld_safe --user=mysql
