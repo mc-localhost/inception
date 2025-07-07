88ec0cf22e1ca3e6f33ffb64c2317160d9cb8b7b
Viktoriia Vasiukova <vvasiuko@student.42heilbronn.de>
2025-07-07 15:06:47 +0200

    more debug messages for wordpress

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd /var/www/html
+echo "i'm in /var/www/html"
 
 # wp-cli.phar (PHP ARchive) is an archive that bundles the entire WP-CLI command-line interface for WordPress
 if [ ! -f wp-cli.phar ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
+	echo "downloaded wp-cli.phar"
 fi
 
 if [ ! -f wp-config.php ]; then
+	echo "no wp-config.php, downloading with wp-cli.phar"
 	./wp-cli.phar core download --allow-root
 	./wp-cli.phar config create \
 		--dbname="$DB_NAME" \
@@ -15,7 +18,7 @@ if [ ! -f wp-config.php ]; then
 		--dbpass="$DB_PASS" \
 		--dbhost="$DB_HOST" \
 		--allow-root
-
+	echo "created config with DB_..."
 	./wp-cli.phar core install \
 		--url="$DOMAIN_NAME" \
 		--title="$WP_TITLE" \
@@ -24,11 +27,12 @@ if [ ! -f wp-config.php ]; then
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--skip-email \
 		--allow-root
-
+	echo "installed core"
 	./wp-cli.phar user create "$WP_USR" "$WP_EMAIL" \
         --user_pass="$WP_PWD" \
         --role=author \
         --allow-root
+	echo "created user WP_USER as author"
 fi
 
 php-fpm7.4 -F
